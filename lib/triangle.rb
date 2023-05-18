45bdcf600645f41c8d763336bb896eae95add25a
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-05-17 21:29:27 -0700

    commit tree 7c941a21c03e17bf6323e7b48f0e901cd85d5349

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_reader :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    sides = [side_1, side_2, side_3].sort
+      if sides[0] <= 0 || sides[0] + sides[1] <= sides[2]
+        raise TriangleError
+      else [:scalene, :isosceles, :equilateral][ 3 - sides.uniq.size ]
+      end
+
+  end
+
+  class TriangleError < StandardError
+    def message
+      "bad triangle!"
+    end
+  end
+
 end
